44acd6ebfffc8901da2c971697b3bf6e5023f343
salvadoradrianG <salvadoradrian4356@gmail.com>
2026-08-15 09:31:27 +0000

    editnote

--- edit_note.py
+++ edit_note.py
@@ -1,12 +1,12 @@
 # Adrian
 notes = []
 
-def edit_note
-    selected = note list.curseselection()
+def edit_note():
+    selected = note_list.curselection()
     
     if selected:
         index = slected[0]
         notes[index] = text_area.get("1.0", "end").strip()
-        update_list{}
+        update_list()
 
 
