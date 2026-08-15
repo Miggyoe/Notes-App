1ff35840f2e121a8cefdded8f195967b08d05397
Ethan Miguel P. Patio <ethanmiguel627@gmail.com>
2026-08-15 09:27:09 +0000

    Added viewnotes

--- add_note.py
+++ add_note.py
@@ -8,3 +8,15 @@ def add_note():
     print(f"Note {title} has been added!")
 
 print(add_note())
+
+def view_notes():
+    if not notes:
+        print("\nNo notes available...")
+        return
+    
+    print("\nYour notes")
+    for idx, note in enumerate(notes, start=1):
+        print(f"{idx}. {note['title']}")
+        print(f"{note['content']}\n")
+
+print(view_notes())
