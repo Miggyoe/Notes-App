f6c9df5dfddd5745802b540687ff045ed2b72e74
Ethan Miguel P. Patio <ethanmiguel627@gmail.com>
2026-08-15 09:14:08 +0000

    Updated addnote

--- add_note.py
+++ add_note.py
@@ -1,10 +1,10 @@
-notes = []q
+notes = []
 
 def add_note():
     title = input("Add note title: ")
     content = input("Add note text: ")
     if title:
-        notes.append({"title": Title, "content": Content})
+        notes.append({"title": title, "content": content})
     print(f"Note {title} has been added!")
 
 print(add_note())
