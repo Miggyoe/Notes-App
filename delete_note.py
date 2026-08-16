58f440dbba42f4c5ded03d03beb45fbbf595d2ba
maunmacky08 <maun.macky08@gmail.com>
2026-08-16 16:05:09 +0000

    gave it functions

--- delete_note.py
+++ delete_note.py
@@ -1,7 +1,23 @@
+notes = []
 
+from notes import notes
 
 
 def delete_note():
     if not notes:
         print("No notes available...")
         return
+    print("Your notes:")
+
+    count = 1
+        for note in notes:
+        print(count, note["title"])
+        count += 1
+    
+    choice = int(input("Select note to delete: "))
+
+    if choice >= 1 and choice <= len(notes):
+        notes.pop(choice - 1)
+        print("Note deleted!")
+    else:
+        print("Invalid note.")
