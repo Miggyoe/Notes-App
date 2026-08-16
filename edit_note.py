41cb72f504dc2e2a6ededbea117fea3b8df5f705
salvadoradrianG <salvadoradrian4356@gmail.com>
2026-08-16 13:46:45 +0000

    Add edit note function

--- edit_note.py
+++ edit_note.py
@@ -1,13 +1,43 @@
 # Adrian
-notes = []
 
 def edit_note():
-   if not notes:
-        print("\nNo notes available..")
+    if not notes:
+        print(30*"=")
+        print("No notes available..")
+        print(30*"=")
         return
 
-    print("\nYour notes")
-    for idx, note in enumerate(notes, start=1):
-        
+    view_note()
+
+    choice = input("Select a note number to edit: ")
+
+    if choice.isdigit():
+        index = int(choice) - 1
+
+        if 0 <= index < len(notes):
+            print(30*"=")
+            print(f"editing note:, {notes[index]["title"]}")
+
+            title = input("enter new title: ")
+            content = input("enter new note text: ")
+
+            if title:
+                notes[index]['title'] = title
+
+            if content:
+                notes[index]['content'] = content
+
+            print(30*"=")
+            print("Notes has been edited! ")
+            print(30*"=")
+
+        else:
+            print("Invalid note number.")
+
+    else:
+        print("Please enter a number.")
+         
+
+
 
 
