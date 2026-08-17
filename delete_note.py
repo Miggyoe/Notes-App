08e1369f7556a1dac23d281926136f6ebc910843
Ethan Miguel P. Patio <ethanmiguel627@gmail.com>
2026-08-17 23:28:05 +0000

    delete_note checkout from macky, delete_note old

--- delete_note.py
+++ delete_note.py
@@ -1,13 +1,23 @@
-
 notes = []
 
-def delete_note
-    selected = note list.curseselection()
-
-    if selected:
-        index = selected[0]
-        notes[index] = text_area.get()
+from notes import notes
 
 
+def delete_note():
+    if not notes:
+        print("No notes available...")
+        return
+    print("Your notes:")
 
-print(input)
+    count = 1
+        for note in notes:
+        print(count, note["title"])
+        count += 1
+    
+    choice = int(input("Select note to delete: "))
+
+    if choice >= 1 and choice <= len(notes):
+        notes.pop(choice - 1)
+        print("Note deleted!")
+    else:
+        print("Invalid note.")
